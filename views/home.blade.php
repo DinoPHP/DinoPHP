77df204806455661ead8a47a95ab0832453e58b8
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-07-22 17:18:13 +0200

    mirror edit

--- views/home.blade.php
+++ views/home.blade.php
@@ -5,6 +5,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700;800;900&display=swap" rel="stylesheet">
+    <link rel="icon" href="{{('pics/DinoPHP-icon.png')}}">
     <style>
         body {
             background: #092531;
